a53c330842b1deebefb2c94c09679312542e94f5
Cole <ce.kasabian@gmail.com>
2023-04-27 21:31:58 -0700

    completed the Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,34 @@
-//TODO create a Schema of REACTION in the thought model as a subdocument
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (timestamp) {
+      return require("moment")(timestamp).format("MMM Do YYYY, h:mm:ss a");
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reaction.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
